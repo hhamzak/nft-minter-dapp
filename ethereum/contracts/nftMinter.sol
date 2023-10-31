c5d190bd182e2756f3943c6acb50841faec91ecb
HAMZA-PC\HAMZA <hasanhamzakaldirim@gmail.com>
2023-10-31 14:17:41 +0300

    changes on compile

---
+++ ethereum/contracts/nftMinter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+//import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import './ERC721A.sol';
+
+contract NFTMinter is ERC721A {
+
+    string [] public mintElements;
+
+    function mint(string memory _imageCid) public returns(uint256) {
+        // this is deprecated - uint _id = KryptoBirdz.push(_kryptoBird);
+        mintElements.push(_imageCid);
+        uint _id = mintElements.length - 1;
+
+        // .push no longer returns the length but a ref to the added element
+        _mint(msg.sender, _id);
+
+        return _id;
+    }
+
+    constructor(string memory name, string memory symbol)
+        ERC721A(name, symbol)
+    {}
+    
+
+// %10 
+// %90
+
+}
